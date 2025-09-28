550f0858e540eb9ce6b71dea8864183b0b33b263
Em1n001 <eminquliyev826@gmail.com>
2025-09-28 19:54:34 +0400

    a

---
+++ Front-end/signup.js
@@ -0,0 +1,7 @@
+//function onLogin(event){
+//    event.preventDefault();
+//    window.location.href='login.html';
+//}
+document.querySelector('button').addEventListener('click', (e) => {
+    window.location.href = 'login.html';
+})
